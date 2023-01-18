406236b427e2b85f96bcc6c1aa20b166eee18ee0
Robyn MacCallum <robyntmaccallum@gmail.com>
2023-01-18 15:38:41 -0500

    Add file upload methods to get around circular dependency issues

---
+++ libs/common/src/services/fileUpload/cipher-file-upload-methods.ts
@@ -0,0 +1,23 @@
+import { ApiService } from "../../abstractions/api.service";
+import { AttachmentUploadDataResponse } from "../../models/response/attachment-upload-data.response";
+import { FileUploadApiMethods } from "../../types/fileUploadApiMethods";
+
+export class CipherFileApiMethods implements FileUploadApiMethods {
+  constructor(private apiService: ApiService) {}
+
+  postFile(cipherId: string, fileId: string, fileData: FormData): Promise<any> {
+    return this.apiService.postAttachmentFile(cipherId, fileId, fileData);
+  }
+
+  renewFileUploadUrl(cipherId: string, fileId: string): Promise<AttachmentUploadDataResponse> {
+    return this.apiService.renewAttachmentUploadUrl(cipherId, fileId);
+  }
+
+  delete(cipherId: string, attachmentId: string, isAdmin: boolean): Promise<any> {
+    if (isAdmin) {
+      return this.apiService.deleteCipherAttachmentAdmin(cipherId, attachmentId);
+    } else {
+      return this.apiService.deleteCipherAttachment(cipherId, attachmentId);
+    }
+  }
+}
