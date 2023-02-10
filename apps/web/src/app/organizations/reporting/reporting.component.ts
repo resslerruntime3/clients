215a8f6213d4f105127a0efa5bdbc4054cbcb67d
Jacob Fink <jfink@bitwarden.com>
2023-02-10 11:25:01 -0500

    [EC-859] transition remaining components

--- apps/web/src/app/organizations/reporting/reporting.component.ts
+++ apps/web/src/app/organizations/reporting/reporting.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { combineLatest, map, Observable, startWith } from "rxjs";
+import { map, Observable, startWith, switchMap } from "rxjs";
 
 import { OrganizationService } from "@bitwarden/common/abstractions/organization/organization.service.abstraction";
 import { Organization } from "@bitwarden/common/models/domain/organization";
@@ -16,11 +16,8 @@ export class ReportingComponent implements OnInit {
   constructor(private route: ActivatedRoute, private organizationService: OrganizationService) {}
 
   ngOnInit() {
-    this.organization$ = combineLatest(
-      [this.route.params, this.organizationService.organizations$],
-      (params, orgs) => {
-        return orgs.find((o) => o.id === params.organizationId);
-      }
+    this.organization$ = this.route.params.pipe(
+      switchMap((params) => this.organizationService.get$(params.organizationId))
     );
 
     this.showLeftNav$ = this.organization$.pipe(
--- apps/web/src/app/organizations/settings/settings.component.ts
+++ apps/web/src/app/organizations/settings/settings.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { combineLatest, Observable } from "rxjs";
+import { Observable, switchMap } from "rxjs";
 
 import { OrganizationService } from "@bitwarden/common/abstractions/organization/organization.service.abstraction";
 import { Organization } from "@bitwarden/common/models/domain/organization";
@@ -15,11 +15,8 @@ export class SettingsComponent implements OnInit {
   constructor(private route: ActivatedRoute, private organizationService: OrganizationService) {}
 
   ngOnInit() {
-    this.organization$ = combineLatest(
-      [this.route.params, this.organizationService.organizations$],
-      (params, orgs) => {
-        return orgs.find((o) => o.id === params.organizationId);
-      }
+    this.organization$ = this.route.params.pipe(
+      switchMap((params) => this.organizationService.get$(params.organizationId))
     );
   }
 }
