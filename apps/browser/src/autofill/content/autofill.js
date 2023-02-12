28f70f44cb9aa09968174517fcf8981f212f20c3
Todd Martin <tmartin@bitwarden.com>
2023-02-12 13:22:21 -0500

    Added to autofill.js.

--- apps/browser/src/autofill/content/autofill.js
+++ apps/browser/src/autofill/content/autofill.js
@@ -370,6 +370,7 @@
               addProp(field, 'onepasswordSectionFieldKind', el.dataset.onepasswordSectionFieldKind);
               addProp(field, 'onepasswordSectionFieldTitle', el.dataset.onepasswordSectionFieldTitle);
               addProp(field, 'onepasswordSectionFieldValue', el.dataset.onepasswordSectionFieldValue);
+              addProp(field, 'formControlName', el.getElementAttrValue('formControlName'));
 
               if (el.form) {
                   field.form = getElementAttrValue(el.form, 'opid');
