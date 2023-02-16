6f3037e2229b758124d5254448a8648d273bd5cd
Jacob Fink <jfink@bitwarden.com>
2023-02-16 15:13:23 -0500

    [EC-1046] add activate autofill policy to web

---
+++ bitwarden_license/bit-web/src/app/policies/activate-autofill.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+
+import { PolicyType } from "@bitwarden/common/enums/policyType";
+import {
+  BasePolicy,
+  BasePolicyComponent,
+} from "@bitwarden/web-vault/app/organizations/policies/base-policy.component";
+
+export class ActivateAutofillPolicy extends BasePolicy {
+  name = "activateAutofill";
+  description = "activateAutofillDesc";
+  type = PolicyType.ActivateAutofill;
+  component = ActivateAutofillPolicyComponent;
+}
+
+@Component({
+  selector: "policy-activate-autofill",
+  templateUrl: "activate-autofill.component.html",
+})
+export class ActivateAutofillPolicyComponent extends BasePolicyComponent {}
