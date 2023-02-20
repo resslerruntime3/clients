ba43a26087bbbaa667af458750a37c791d52400f
Thomas Rittson <31796059+eliykat@users.noreply.github.com>
2023-02-21 08:57:57 +1000

    [EC-1057] Add data loss warning to notificationBar edit flow (#4761)

--- apps/browser/src/vault/popup/components/vault/add-edit.component.ts
+++ apps/browser/src/vault/popup/components/vault/add-edit.component.ts
@@ -130,15 +130,11 @@ export class AddEditComponent extends BaseAddEditComponent {
           : tabs.filter((tab) => tab.url != null && tab.url !== "").map((tab) => tab.url);
     }
 
-    window.setTimeout(() => {
-      if (!this.editMode) {
-        if (this.cipher.name != null && this.cipher.name !== "") {
-          document.getElementById("loginUsername").focus();
-        } else {
-          document.getElementById("name").focus();
-        }
-      }
-    }, 200);
+    this.setFocus();
+
+    if (this.popupUtilsService.inTab(window)) {
+      this.popupUtilsService.enableCloseTabWarning();
+    }
   }
 
   async load() {
@@ -155,6 +151,7 @@ export class AddEditComponent extends BaseAddEditComponent {
     }
 
     if (this.popupUtilsService.inTab(window)) {
+      this.popupUtilsService.disableCloseTabWarning();
       this.messagingService.send("closeTab", { delay: 1000 });
       return true;
     }
@@ -246,5 +243,19 @@ export class AddEditComponent extends BaseAddEditComponent {
           ? []
           : this.collections.filter((c) => (c as any).checked).map((c) => c.id),
     });
+  }
+
+  private setFocus() {
+    window.setTimeout(() => {
+      if (this.editMode) {
+        return;
+      }
+
+      if (this.cipher.name != null && this.cipher.name !== "") {
+        document.getElementById("loginUsername").focus();
+      } else {
+        document.getElementById("name").focus();
+      }
+    }, 200);
   }
 }
