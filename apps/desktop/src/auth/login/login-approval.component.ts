15980f62444cb8241e64879bc634d8b6d9c4ea16
André Bispo <abispo@bitwarden.com>
2023-02-23 10:24:13 +0000

    [PM-108] Change userId to userEmail. Remove fingerprint from AuthResponse

--- apps/desktop/src/auth/login/login-approval.component.ts
+++ apps/desktop/src/auth/login/login-approval.component.ts
@@ -6,14 +6,13 @@ import { ModalRef } from "@bitwarden/angular/components/modal/modal.ref";
 import { ModalConfig } from "@bitwarden/angular/services/modal.service";
 import { ApiService } from "@bitwarden/common/abstractions/api.service";
 import { AppIdService } from "@bitwarden/common/abstractions/appId.service";
+import { CryptoService } from "@bitwarden/common/abstractions/crypto.service";
 import { I18nService } from "@bitwarden/common/abstractions/i18n.service";
 import { PlatformUtilsService } from "@bitwarden/common/abstractions/platformUtils.service";
 import { StateService } from "@bitwarden/common/abstractions/state.service";
 import { AuthService } from "@bitwarden/common/auth/abstractions/auth.service";
 import { AuthRequestResponse } from "@bitwarden/common/auth/models/response/auth-request.response";
 import { Utils } from "@bitwarden/common/misc/utils";
-
-import { CryptoService } from './../../../../../libs/common/src/services/crypto.service';
 
 const RequestTimeOut = 60000 * 15; //15 Minutes
 const RequestTimeUpdate = 60000 * 5; //5 Minutes
@@ -67,8 +66,8 @@ export class LoginApprovalComponent implements OnInit, OnDestroy {
     if (this.notificationId != null) {
       this.authRequestResponse = await this.apiService.getAuthRequest(this.notificationId);
       const publicKey = Utils.fromB64ToArray(this.authRequestResponse.publicKey);
-      this.fingerprintPhrase =  (await this.cryptoService.getFingerprint(await this.stateService.getUserId(), publicKey.buffer)).join('-');
       this.email = await this.stateService.getEmail();
+      this.fingerprintPhrase =  (await this.cryptoService.getFingerprint(this.email, publicKey.buffer)).join('-');
       this.updateTimeText();
 
       this.interval = setInterval(() => {
