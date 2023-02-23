84becb8b76c27c9b3958d9805ace8aa8030737a3
Rui Tome <rtome@bitwarden.com>
2023-02-23 16:27:50 +0000

    [AC-431] Added properties 'key' and 'keys' to OrganizationUserAcceptRequest

--- libs/common/src/abstractions/organization-user/requests/organization-user-accept.request.ts
+++ libs/common/src/abstractions/organization-user/requests/organization-user-accept.request.ts
@@ -1,5 +1,9 @@
+import { OrganizationKeysRequest } from "../../../models/request/organization-keys.request";
+
 export class OrganizationUserAcceptRequest {
   token: string;
   // Used to auto-enroll in master password reset
   resetPasswordKey: string;
+  key: string;
+  keys: OrganizationKeysRequest;
 }
