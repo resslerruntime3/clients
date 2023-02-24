f4b94caf08e3b67d83315af441ea3abb922c55e1
Colton Hurst <colton@coltonhurst.com>
2023-02-24 15:49:48 -0500

    SM-528: Add admin check on import / export

--- bitwarden_license/bit-web/src/app/secrets-manager/settings/settings-routing.module.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/settings/settings-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
+import { Provider } from "@bitwarden/common/models/domain/provider";
+
+import { ProviderPermissionsGuard } from "../../providers/guards/provider-permissions.guard";
+
 import { SecretsManagerExportComponent } from "./porting/sm-export.component";
 import { SecretsManagerImportComponent } from "./porting/sm-import.component";
 
@@ -8,15 +12,19 @@ const routes: Routes = [
   {
     path: "import",
     component: SecretsManagerImportComponent,
+    canActivate: [ProviderPermissionsGuard],
     data: {
       titleId: "importData",
+      providerPermissions: (provider: Provider) => provider.isProviderAdmin,
     },
   },
   {
     path: "export",
     component: SecretsManagerExportComponent,
+    canActivate: [ProviderPermissionsGuard],
     data: {
       titleId: "exportData",
+      providerPermissions: (provider: Provider) => provider.isProviderAdmin,
     },
   },
 ];
